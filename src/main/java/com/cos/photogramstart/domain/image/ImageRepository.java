7f1cf546e7fc62c01f24947b06369bf1dee1e509
rkdwlss2 <rkdwlss2@gmail.com>
2024-02-08 14:01:43 +0900

    설정파일 변경

--- src/main/java/com/cos/photogramstart/domain/image/ImageRepository.java
+++ src/main/java/com/cos/photogramstart/domain/image/ImageRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public interface ImageRepository extends JpaRepository<Image,Integer> {
 
-    @Query(value = "SELECT * FROM image WHERE userId IN (SELECT toUserId FROM subscribe WHERE fromUserId = :principalId) ORDER BY id DESC",nativeQuery = true)
+    @Query(value = "SELECT * FROM image WHERE userId IN (SELECT toUserId FROM Subscribe WHERE fromUserId = :principalId) ORDER BY id DESC",nativeQuery = true)
     Page<Image> mStory(int principalId, Pageable pageable);
 
     @Query(value = "SELECT i.* FROM image i INNER JOIN (SELECT imageId, COUNT(imageId) likeCount FROM likes GROUP BY imageId) c on i.id = c.imageId ORDER BY likeCount DESC", nativeQuery = true)
